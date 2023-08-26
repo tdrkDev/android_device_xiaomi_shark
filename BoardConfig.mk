6b39290712b39be6f446ff92f7d2627ecc5e8118
Bruno Martins <bgcngm@gmail.com>
2023-08-26 20:27:03 +0700

    shark: Replace BTM_DEF_LOCAL_NAME with a sysprop
    Change-Id: I2c90efac01e61500eb1be23bb0ae6f96c2acdc53
    Signed-off-by: Andrey Sprynga <rtx4d@aospa.co>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,6 +34,7 @@ BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Recovery
 BOARD_USES_RECOVERY_AS_BOOT := true
