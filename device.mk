89e6dd9ee603a52bb2d5f55c800180c357d1bdbe
Andrey Sprynga <rtx4d@aospa.co>
2023-10-28 16:00:54 +0700

    shark: overlay: Add notification LED capabilities
    Signed-off-by: Andrey Sprynga <rtx4d@aospa.co>

--- device.mk
+++ device.mk
@@ -72,7 +72,8 @@ PRODUCT_USE_VNDK_OVERRIDE := true
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Permissions
 PRODUCT_COPY_FILES += \
